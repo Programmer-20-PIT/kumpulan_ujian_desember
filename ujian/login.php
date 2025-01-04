41939b26624a73509079d8b07c7fc055716e1e49
IrulesDev <ad4112ul@gmail.com>
2025-01-04 14:39:09 +0700

    tambah style nih

--- ujian/login.php
+++ ujian/login.php
@@ -23,6 +23,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/login.css">
     <title>Login</title>
 </head>
 <body>
